ec0ab7022fed678e24274d511a76d1680ee2aa22
Richard Garcia <code.richardy@gmail.com>
2025-11-21 22:58:52 -0300

    this commit introduces a fluent mechanism to apply modifications to establishments and initializes the collection of integration credentials

--- Source/Vinder.Comanda.Stores.Domain/Entities/Establishment.cs
+++ Source/Vinder.Comanda.Stores.Domain/Entities/Establishment.cs
@@ -5,4 +5,8 @@ public sealed class Establishment : Entity
     public Properties Properties { get; set; } = default!;
     public Branding Branding { get; set; } = default!;
     public User Owner { get; set; } = default!;
+    public ICollection<IntegrationCredentials> Credentials { get; set; } = [];
+
+    public void WithChanges(Action<EstablishmentBuilder> action) =>
+        action(new EstablishmentBuilder(this));
 }
