776417b44eb56e46cfbfc80efeb447d5c3141fa4
Richard Garcia <code.richardy@gmail.com>
2025-11-21 22:58:01 -0300

    this commit introduces a fluent API for modifying establishment aggregates while preserving existing data

---
+++ Source/Vinder.Comanda.Stores.Domain/Builders/EstablishmentBuilder.cs
@@ -0,0 +1,23 @@
+﻿namespace Vinder.Comanda.Stores.Domain.Builders;
+
+public sealed class EstablishmentBuilder(Establishment establishment)
+{
+    private readonly Establishment _establishment = establishment;
+
+    public EstablishmentBuilder SetCredential(IntegrationCredentials credential)
+    {
+        _establishment.Credentials.Add(credential);
+        return this;
+    }
+
+    public EstablishmentBuilder SetBranding(Branding branding)
+    {
+        _establishment.Branding = branding;
+        return this;
+    }
+
+    public Establishment Build()
+    {
+        return _establishment;
+    }
+}
