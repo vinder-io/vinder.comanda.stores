fbf25c3c278b8d9f71dfed6a9b66703b61c50afe
Richard Garcia <code.richardy@gmail.com>
2025-11-21 22:59:39 -0300

    this commit introduces a model to capture provider-specific credentials used by establishments

---
+++ Source/Vinder.Comanda.Stores.Domain/Entities/IntegrationCredentials.cs
@@ -0,0 +1,7 @@
+﻿namespace Vinder.Comanda.Stores.Domain.Entities;
+
+public sealed class IntegrationCredentials : Entity
+{
+    public IntegrationTarget Provider { get; set; } = default!;
+    public string SecretKey { get; set; } = default!;
+}
