8a2e34461a7fb575bd3fa46e8f949de86ae0a24a
Richard Garcia <code.richardy@gmail.com>
2025-11-21 22:55:36 -0300

    this commit introduces the mapping layer that transforms integration credential inputs into domain entities and exposes them as response models

---
+++ Source/Vinder.Comanda.Stores.Application/Mappers/IntegrationCredentialsMapper.cs
@@ -0,0 +1,17 @@
+﻿namespace Vinder.Comanda.Stores.Application.Mappers;
+
+public static class IntegrationCredentialsMapper
+{
+    public static IntegrationCredentials AsCredential(this IntegrationCredentialCreationScheme credentials) => new()
+    {
+        Provider = credentials.Provider,
+        SecretKey = credentials.SecretKey,
+    };
+
+    public static IntegrationCredentialScheme AsResponse(this IntegrationCredentials credentials) => new()
+    {
+        Identifier = credentials.Id,
+        Provider = credentials.Provider.ToString(),
+        SecretKey = credentials.SecretKey,
+    };
+}
