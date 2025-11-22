e7985a18210ab4a08e547a8c8a477a9f5cbc55e1
Richard Garcia <code.richardy@gmail.com>
2025-11-21 23:02:00 -0300

    this commit introduces a repository implementation for persisting and retrieving integration credentials in the database

---
+++ Source/Vinder.Comanda.Stores.Infrastructure/Repositories/IntegrationCredentialsRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace Vinder.Comanda.Stores.Infrastructure.Repositories;
+
+public sealed class IntegrationCredentialsRepository(IMongoDatabase database) :
+    BaseRepository<IntegrationCredentials>(database, Collections.Credentials),
+    IIntegrationCredentialsRepository;
