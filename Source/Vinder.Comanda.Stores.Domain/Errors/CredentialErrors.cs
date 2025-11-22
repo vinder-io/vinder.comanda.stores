098253b1130a5789c11cd02e536bddf35a599a27
Richard Garcia <code.richardy@gmail.com>
2025-11-22 11:44:29 -0300

    this commit introduces a domain error for missing credentials, providing a standardized code and description for not found scenarios

---
+++ Source/Vinder.Comanda.Stores.Domain/Errors/CredentialErrors.cs
@@ -0,0 +1,9 @@
+﻿namespace Vinder.Comanda.Stores.Domain.Errors;
+
+public sealed record CredentialErrors
+{
+    public static readonly Error CredentialDoesNotExist = new(
+        Code: "#COMANDA-ERROR-2147F",
+        Description: "the specified credential does not exist."
+    );
+}
