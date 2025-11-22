dc7f9b79e9c33a609cd0f9e14b6a6a60975ed95e
Richard Garcia <code.richardy@gmail.com>
2025-11-21 22:53:47 -0300

    this commit introduces the fetch parameters record for retrieving integration credentials scoped to an establishment

---
+++ Source/Vinder.Comanda.Stores.Application/Payloads/Establishment/IntegrationCredentialsFetchParameters.cs
@@ -0,0 +1,7 @@
+﻿namespace Vinder.Comanda.Stores.Application.Payloads.Establishment;
+
+public sealed record IntegrationCredentialsFetchParameters :
+    IMessage<Result<IEnumerable<IntegrationCredentialScheme>>>
+{
+    public string EstablishmentId { get; init; } = default!;
+}
