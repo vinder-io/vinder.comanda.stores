a9687ebb00d1ece5619a5bed50da48805fd41715
Richard Garcia <code.richardy@gmail.com>
2025-11-22 11:44:16 -0300

    this commit introduces a withChanges method to integration credentials, enabling fluent updates through the builder pattern

--- Source/Vinder.Comanda.Stores.Domain/Entities/IntegrationCredentials.cs
+++ Source/Vinder.Comanda.Stores.Domain/Entities/IntegrationCredentials.cs
@@ -4,4 +4,7 @@ public sealed class IntegrationCredentials : Entity
 {
     public IntegrationTarget Provider { get; set; } = default!;
     public string SecretKey { get; set; } = default!;
+
+    public void WithChanges(Action<IntegrationCredentialBuilder> action) =>
+        action(new IntegrationCredentialBuilder(this));
 }
