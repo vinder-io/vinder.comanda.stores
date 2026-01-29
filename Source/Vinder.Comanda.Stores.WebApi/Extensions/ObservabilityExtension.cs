58cbed6afe7059a3f0e1450c3d85544cdc0fa2e0
Richard Garcia <code.richardy@gmail.com>
2026-01-29 20:37:27 -0300

    feat(#2): this commit introduces observability extension for serilog integration in the application

---
+++ Source/Vinder.Comanda.Stores.WebApi/Extensions/ObservabilityExtension.cs
@@ -0,0 +1,23 @@
+namespace Vinder.Comanda.Stores.WebApi.Extensions;
+
+[ExcludeFromCodeCoverage(Justification = "contains only http pipeline configuration with no business logic.")]
+public static class ObservabilityExtension
+{
+    public static void AddObservability(this WebApplicationBuilder builder)
+    {
+        if (!builder.Environment.IsDevelopment() || !builder.Environment.IsProduction())
+            return;
+
+        builder.Host.UseSerilog((context, services, logger) =>
+        {
+            var settings = services.GetRequiredService<ISettings>();
+
+            logger
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Seq(settings.Observability.SeqServerUrl);
+        });
+    }
+}
