2605d51641ce208cf4b0065d1c230aabaa3f787d
Richard Garcia <code.richardy@gmail.com>
2026-01-29 20:39:23 -0300

    feat(#2): this commit introduces monitoring extension for sentry integration in the application

---
+++ Source/Vinder.Comanda.Stores.WebApi/Extensions/MonitoringExtension.cs
@@ -0,0 +1,24 @@
+namespace Vinder.Comanda.Stores.WebApi.Extensions;
+
+[ExcludeFromCodeCoverage(Justification = "contains only http pipeline configuration with no business logic.")]
+public static class MonitoringExtension
+{
+    public static void AddMonitoring(this WebApplicationBuilder builder)
+    {
+        if (!builder.Environment.IsDevelopment() || !builder.Environment.IsProduction())
+            return;
+
+        var settings = builder.Services
+            .BuildServiceProvider()
+            .GetRequiredService<ISettings>();
+
+        // https://docs.sentry.io/platforms/dotnet/
+        builder.WebHost.UseSentry(options =>
+        {
+            options.Dsn = settings.Observability.SentryDsn;
+            options.Environment = builder.Environment.EnvironmentName;
+            options.TracesSampleRate = 1.0;
+            options.Debug = true;
+        });
+    }
+}
