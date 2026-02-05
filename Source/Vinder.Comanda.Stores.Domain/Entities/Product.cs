9e1ed02ce90791359fb5b756b6c3dba91626af83
Richard Garcia <code.richardy@gmail.com>
2026-02-04 22:34:15 -0300

    refactor: remove Product entity as it is no longer needed

--- Source/Vinder.Comanda.Stores.Domain/Entities/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Vinder.Comanda.Stores.Domain.Entities;
-
-public sealed class Product : Entity
-{
-    public Image Image { get; set; } = default!;
-    public Price Price { get; set; } = default!;
-    public Properties Properties { get; set; } = default!;
-    public ProductMetadata Metadata { get; set; } = default!;
-}
