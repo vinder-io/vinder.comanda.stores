d9182a9999fcf346322a8db4c736c1d2248966b1
Richard Garcia <code.richardy@gmail.com>
2026-02-04 22:34:05 -0300

    feat: add Product aggregate with properties and methods

---
+++ Source/Vinder.Comanda.Stores.Domain/Aggregates/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Vinder.Comanda.Stores.Domain.Aggregates;
+
+public sealed class Product : Aggregate
+{
+    public Image Image { get; set; } = default!;
+    public Price Price { get; set; } = default!;
+    public Properties Properties { get; set; } = default!;
+    public ProductMetadata Metadata { get; set; } = default!;
+}
