83bb26ac7d21129641c9bea264b4280736793e91
Richard Garcia <code.richardy@gmail.com>
2026-02-04 22:33:58 -0300

    feat: add Credential aggregate with properties and method for changes

---
+++ Source/Vinder.Comanda.Stores.Domain/Aggregates/Credential.cs
@@ -0,0 +1,10 @@
+﻿namespace Vinder.Comanda.Stores.Domain.Aggregates;
+
+public sealed class Credential : Aggregate
+{
+    public IntegrationTarget Provider { get; set; } = default!;
+    public string SecretKey { get; set; } = default!;
+
+    public void WithChanges(Action<CredentialBuilder> action) =>
+        action(new CredentialBuilder(this));
+}
